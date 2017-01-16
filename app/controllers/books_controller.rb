2aa1df413a74b73ea69c991efb3628410fb140a7
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-01-15 20:18:12 -0500

    create and destroy to app

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+  def index
+      @books = Book.all
+      render :index
+  end
+
+
+  def new
+    render :new
+  end
+
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_url
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_url
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
